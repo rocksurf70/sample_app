f3fba8321d8fab6eecd609917a723f85c37180de
John Millar <john_millar70@hotmail.com>
2012-02-03 18:28:56 +1100

    end of day

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -7,5 +8,28 @@ class User < ActiveRecord::Base
             :length => { :maximum => 50 }
   validates :email, :presence => true,
             :format => { :with => email_regex },
-  :uniqueness => { :case_sensitive => false }
+            :uniqueness => { :case_sensitive => false }
+
+  validates :password, :presence => true,
+            :confirmation => true,
+            :length => { :within => 6..40 }
+
+  before_save :encrypt_password
+
+  # Return true if the user's password matches the submittted password.
+  def has_password?(submitted_password)
+    # Compare encrypted_password with the encrypted version of
+    # submitted_password
+  end
+
+  private
+  
+  def encrypt_password
+    self.encrypted_password = encrypt(password)
+  end
+
+  def encrypt(string)
+    string # Only a temporary implementation
+  end
+
 end
